7a2ca21a41cea522080aa1ec5a564c2426580f7e
Zain Wania <zain@wania.dev>
2023-09-11 19:28:25 -0400

    init

---
+++ src/data/info.ts
@@ -0,0 +1,88 @@
+export const info = {
+  baseUrl: "https://zainresu.me",
+  name: "Zain Wania",
+  jobDescription: "Full Stack Developer",
+  about: `and I am a highly skilled Full Stack Web Developer with 8 years of experience. I specialize in utilizing innovative technologies and frameworks such as Vue, Nuxt, Ruby on Rails, and Typescript to develop robust and user-friendly web applications. My expertise extends to cloud platforms like AWS, as well as deployment services like Netlify and Vercel. With a strong background in database management using MySQL and Planetscale, I have successfully delivered scalable and efficient solutions. I am passionate about creating exceptional user experiences and continuously staying updated with the latest industry trends. I am always seeking new opportunities to apply my expertise and contribute to cutting-edge web development projects`,
+
+  experience: [
+    {
+      name: "Contactmonkey",
+      location: "Toronto, Ontario, Canada",
+      startDate: "Oct 2019",
+      endDate: "Present",
+      description: [
+        "- Migration of the frontend from Ruby on Rails to a modern Vue.js SPA.",
+        "- Lead the development of numerous features and improvements to the platform.",
+        "- Implemented a new design system and component library.",
+        "- Implemented a Monorepo structure for the frontend.",
+      ],
+    },
+
+    {
+      name: "Ample Organics",
+      location: "Toronto, Ontario, Canada",
+      startDate: "Jan 2018",
+      endDate: "Sep 2019",
+      description: [
+        "- Lead the development of a new frontend for the company's flagship product.",
+        "- Implemented a new design system and component library.",
+        "- Led the implementation of E2E testing strategies, resulting in improved product reliability, efficient bug detection and saving of technical resources."
+      ],
+    },
+
+    {
+      name: "Exact Media",
+      location: "Toronto, Ontario, Canada",
+      startDate: "Feb 2016",
+      endDate: "Apr 2017",
+      description: [
+        "- Solo launched a new product from scratch, which supported over 70,000 users",
+        "- Deployed and maintained a Ruby on Rails application on AWS",
+      ],
+    },
+  ],
+
+  // education: [
+  //   {
+  //     name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
+  //     location: "Karāchi, Sindh, Pakistan",
+  //     startDate: "2019",
+  //     endDate: "2022",
+  //     description: [
+  //       "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+  //     ],
+  //   },
+  //   {
+  //     name: "Govt. Degree Boys College Buffer Zone",
+  //     location: "Karāchi, Sindh, Pakistan",
+  //     startDate: "2018",
+  //     endDate: "2020",
+  //     description: ["Intermediate, Pre-Engineering"],
+  //   },
+  //   {
+  //     name: "Ghazi Foundation Schools & Colleges",
+  //     location: "Karāchi, Sindh, Pakistan",
+  //     startDate: "2016",
+  //     endDate: "2018",
+  //     description: ["Matriculation,  Computer Science"],
+  //   },
+  // ],
+
+  socialMedia: {
+    // facebook: "https://www.facebook.com/profile.php?id=100022899849583",
+    twitter: "https://twitter.com/Zain_Wania",
+    github: "https://github.com/ZainW",
+    email: "zainthegreatcb@gmail.com",
+    linkedin: "https://www.linkedin.com/in/zainw/",
+  },
+
+  projects: [
+    {
+      title: "Zuxt",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/ZainW/zuxt",
+      liveUrl: "https://github.com/ZainW/zuxt",
+    },
+  ],
+};
