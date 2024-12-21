5d5667a18608e10ff51832c4dfaf86e9f32615c6
Zain Wania <zain@wania.dev>
2024-12-20 19:12:41 -0500

    remove buzzwords

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,14 +2,12 @@ export const info = {
   baseUrl: "https://zainresu.me",
   name: "Zain Wania",
   jobDescription: "Full Stack Developer",
-  about: `I am a highly skilled Full Stack Web Developer with 8 years of experience.
-
-  I specialize in utilizing innovative technologies and frameworks such as Vue, Nuxt, Ruby on Rails, and Typescript to develop robust and user-friendly web applications.
-  My expertise extends to cloud platforms like AWS, as well as deployment services like Netlify and Vercel.
-  With a strong background in database management using MySQL and Planetscale, I have successfully delivered scalable and efficient solutions.
-  I am passionate about creating exceptional user experiences and continuously staying updated with the latest industry trends.
-  I am always seeking new opportunities to apply my expertise and contribute to cutting-edge web development projects.
-  Recently, I have been intensly interested in zero by rocicorp, a new sync engine for building better webapps`,
+  about: `I am a Full Stack Web Developer with 8 years of experience.
+  I use technologies like Vue, Nuxt, React, and Typescript to build web applications.
+  I implement responsive designs using Tailwind CSS.
+  I work with cloud platforms like AWS and cloudflare for infrastructure.
+  I have experience with database management using MySQL, Postgres and Planetscale.
+  I am interested in Zero by Rocicorp, a new sync engine for web apps.`,
 
   experience: [
     {
@@ -19,11 +17,11 @@ export const info = {
       startDate: "Oct 2019",
       endDate: "Present",
       description: [
-        "- Migration of the frontend from Ruby on Rails to a modern Vue.js SPA.",
-        "- Lead the development of numerous features and improvements to the platform.",
-        "- Implemented a new design system and component library.",
-        "- Implemented a Monorepo structure for the frontend.",
-        "- Run and mentor the frontend team.",
+        "- Migrated the frontend from Ruby on Rails to Vue.js SPA.",
+        "- Developed features and improvements.",
+        "- Implemented a design system and component library.",
+        "- Set up a Monorepo structure for the frontend.",
+        "- Mentored the frontend team.",
       ],
     },
 
@@ -34,9 +32,9 @@ export const info = {
       startDate: "Jan 2018",
       endDate: "Sep 2019",
       description: [
-        "- Lead the development of a new frontend for the company's flagship product.",
-        "- Implemented a new design system and component library.",
-        "- Led the implementation of E2E testing strategies, resulting in improved product reliability, efficient bug detection and saving of technical resources."
+        "- Developed a new frontend for the main product.",
+        "- Implemented a design system and component library.",
+        "- Led E2E testing strategies, improving product reliability.",
       ],
     },
 
@@ -47,45 +45,18 @@ export const info = {
       startDate: "Feb 2016",
       endDate: "Apr 2017",
       description: [
-        "- Solo launched a new product from scratch, which supported over 70,000 users",
-        "- Deployed and maintained a Ruby on Rails application on AWS",
+        "- Launched a new product from scratch, supporting over 70,000 users.",
+        "- Deployed and maintained a Ruby on Rails application on AWS.",
       ],
     },
   ],
 
-  // education: [
-  //   {
-  //     name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-  //     location: "Karāchi, Sindh, Pakistan",
-  //     startDate: "2019",
-  //     endDate: "2022",
-  //     description: [
-  //       "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
-  //     ],
-  //   },
-  //   {
-  //     name: "Govt. Degree Boys College Buffer Zone",
-  //     location: "Karāchi, Sindh, Pakistan",
-  //     startDate: "2018",
-  //     endDate: "2020",
-  //     description: ["Intermediate, Pre-Engineering"],
-  //   },
-  //   {
-  //     name: "Ghazi Foundation Schools & Colleges",
-  //     location: "Karāchi, Sindh, Pakistan",
-  //     startDate: "2016",
-  //     endDate: "2018",
-  //     description: ["Matriculation,  Computer Science"],
-  //   },
-  // ],
-
   socialMedia: {
-    // facebook: "https://www.facebook.com/profile.php?id=100022899849583",
     twitter: "https://twitter.com/Zain_Wania",
     github: "https://github.com/ZainW",
     email: "zain@wania.dev",
     linkedin: "https://www.linkedin.com/in/zainw/",
-    bluesky: "https://bsky.app/profile/zainwania.bsky.social" // Add this line
+    bluesky: "https://bsky.app/profile/zainwania.bsky.social"
   },
 
   projects: [
